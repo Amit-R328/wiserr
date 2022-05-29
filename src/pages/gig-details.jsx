17533e679f73d5956fa66ccbcb5bf5820025ad80
Sharon Bello <sharonbellotech@gmail.com>
2022-05-29 11:23:45 +0300

    homepage-margn carousel fixed

--- src/pages/gig-details.jsx
+++ src/pages/gig-details.jsx
@@ -26,60 +26,58 @@ export const GigDetails = (props) => {
         props.history.push('/categories')
     }
 
-    if (!gig) return <h1>Loading</h1>
-<<<<<<< HEAD
-    console.log('Gig:', gig._id)
-    let whatYouGet
-    if (gig.description.whatDoYouGet) {
+    // if (!gig) return <h1>Loading</h1>
+    // console.log('Gig:', gig._id)
+    // let whatYouGet
+    // if (gig.description.whatDoYouGet) {
 
-        whatYouGet = gig.description.whatDoYouGet.split('\n')
-    }
+    //     whatYouGet = gig.description.whatDoYouGet.split('\n')
+    // }
 
-    return (
-        <React.Fragment>
-            <div className="app-header">
-                <div className="main-header sticky">
-                    {/* {offset ?
-                        <CategoriesNavHeader style={{ visibility: 'visible' }}
-                            onChangeCategory={onChangeCategory} />
-                        : <CategoriesNavHeader style={{ visibility: 'hidden' }}
-                            onChangeCategory={onChangeCategory} />} */}
-                    <AppHeader />
-                    <span className="line-sep"></span>
-                    <CategoriesNavHeader />
-                    <span className="line-sep"></span>
-                </div>
-            </div>
-            <section className="gig-details flex">
-                <div className="left-container">
-                    <section className="gig-info">
-                        <h3 className="text-display">{gig.title}</h3>
-                        <div className="owner-details flex">
-                            <img className="sml-round-img" src={`${gig.owner.imgUrl}`} alt="" /> &nbsp;
-                            <p className="owner-name">&nbsp;{gig.owner.fullName} &nbsp;</p>
-                        </div>
-                        <div className="img-container">
-                            <img className="gig-details-img img" src={`${gig.imgUrl[0]}`} alt="" />
-                        </div>
-                    </section>
-                    <section className="about-details">
-                        <h2 className="about-title">About this Gig</h2>
-                        <p className="about-this-gig">{gig.description.aboutThisGig}</p>
-                        <br />
-                        {gig.description.whatDoYouGet} && <p className="what-do-you-get">
-                            <dl>
+    // return (
+    //     <React.Fragment>
+    //         <div className="app-header">
+    //             <div className="main-header sticky">
+    //                 {/* {offset ?
+    //                     <CategoriesNavHeader style={{ visibility: 'visible' }}
+    //                         onChangeCategory={onChangeCategory} />
+    //                     : <CategoriesNavHeader style={{ visibility: 'hidden' }}
+    //                         onChangeCategory={onChangeCategory} />} */}
+    //                 <AppHeader />
+    //                 <span className="line-sep"></span>
+    //                 <CategoriesNavHeader />
+    //                 <span className="line-sep"></span>
+    //             </div>
+    //         </div>
+    //         <section className="gig-details flex">
+    //             <div className="left-container">
+    //                 <section className="gig-info">
+    //                     <h3 className="text-display">{gig.title}</h3>
+    //                     <div className="owner-details flex">
+    //                         <img className="sml-round-img" src={`${gig.owner.imgUrl}`} alt="" /> &nbsp;
+    //                         <p className="owner-name">&nbsp;{gig.owner.fullName} &nbsp;</p>
+    //                     </div>
+    //                     <div className="img-container">
+    //                         <img className="gig-details-img img" src={`${gig.imgUrl[0]}`} alt="" />
+    //                     </div>
+    //                 </section>
+    //                 <section className="about-details">
+    //                     <h2 className="about-title">About this Gig</h2>
+    //                     <p className="about-this-gig">{gig.description.aboutThisGig}</p>
+    //                     <br />
+    //                     {gig.description.whatDoYouGet} && <p className="what-do-you-get">
+    //                         <dl>
 
-                                {whatYouGet && whatYouGet.map(line => <dd>{line}</dd>)}
-                            </dl>
-                        </p>
-                        <article className="about-seller">
-                            <p className="about-title">About The Seller</p>
-                            <div className="owner-card flex">
-                                <img className="md-round-img" src={`${gig.owner.imgUrl}`} alt="" />
-                                <div className="owner-card-right flex">
-                                    <p>{gig.owner.fullName}</p>
-                                    <p><i className="fa fa-star filled">
-=======
+    //                             {whatYouGet && whatYouGet.map(line => <dd>{line}</dd>)}
+    //                         </dl>
+    //                     </p>
+    //                     <article className="about-seller">
+    //                         <p className="about-title">About The Seller</p>
+    //                         <div className="owner-card flex">
+    //                             <img className="md-round-img" src={`${gig.owner.imgUrl}`} alt="" />
+    //                             <div className="owner-card-right flex">
+    //                                 <p>{gig.owner.fullName}</p>
+    //                                 <p><i className="fa fa-star filled">
 
     
     let whatYouGet = gig.description.whatDoYouGet.split('\n')
@@ -115,7 +113,6 @@ export const GigDetails = (props) => {
                                 <p>{gig.owner.fullName}</p>
                                 <p><i className="fa fa-star filled">
                                     <i className="fa fa-star filled">
->>>>>>> 2b1ea0bb0bc3a4a113b6777b6643a6b7d1c887e8
                                         <i className="fa fa-star filled">
                                             <i className="fa fa-star filled">
                                                 <i className="fa fa-star filled">
