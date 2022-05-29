7bd8b6186b4a38f35ac246f2d2ccf0808297cc8f
Sharon Bello <sharonbellotech@gmail.com>
2022-05-29 18:58:42 +0300

    details

--- src/cmps/headers/app-header.jsx
+++ src/cmps/headers/app-header.jsx
@@ -13,49 +13,50 @@ export const AppHeader = (props) => {
     return (
         <div className="header">
             <header className="logged-out-header">
-            <div className="header-row-wrapper">
-                <div className="header-row max-width-container equal-padding row-main">
-                    <button className="btn-navicon">
-                        <HamburgerMenu />
-                    </button>
-                    <div className="logo-main-header">
-                        <NavLink to="/" className="site-logo">
-                            <LogoFull />
-                            {/* <LogoFullWhite /> */}
-                        </NavLink>
-                    </div>
-                    <div className="wiserr-header-search-animated">
-                        <div className="search-bar-package-container">
-                            <form className="search-bar-package search_bar-package">
-                                <span className="search-bar-icon searching-icon" aria-hidden="true">
-                                    <SearchBar />
-                                </span>
-                                <Search />
-                                {/* <input type="search" autoComplete="off" placeholder="Find services" value="" />
+                <div className="header-row-wrapper">
+                    <div className="header-row max-width-container equal-padding row-main">
+                        <button className="btn-navicon">
+                            <HamburgerMenu />
+                        </button>
+                        <div className="logo-main-header">
+                            <NavLink to="/" className="site-logo">
+                                <LogoFull />
+                                {/* <LogoFullWhite /> */}
+                            </NavLink>
+                        </div>
+                        <div className="wiserr-header-search-animated">
+                            <div className="search-bar-package-container">
+                                <form className="search-bar-package search_bar-package">
+                                    <span className="search-bar-icon searching-icon" aria-hidden="true">
+                                        <SearchBar />
+                                    </span>
+                                    <Search />
+                                    {/* <input type="search" autoComplete="off" placeholder="Find services" value="" />
                                     <button className="co-white submit-button bg-co-green-700">Search</button> */}
-                            </form>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="wiserr-nav-right-container-helper">
+                            <div className="wiserr-nav wiserr-nav-right">
+                                <ul className="nav-list" >
+
+                                    <li className="display-from-md">
+                                        <NavLink to="/business" className="business-nav-link nav-link">Wiserr Business</NavLink>
+                                    </li>
+
+                                    <li className="display-from-md">
+                                        <div className="resources-tab-package">
+                                            <NavLink to="/categories" className="explore-nav-link nav-link">Explore</NavLink>
+                                        </div>
+                                    </li>
+
+                                    <li className="display-from-sm">
+                                        <NavLink to="/login" rel="nofollow" className="js-open-popup-login nav-link">Login/Join</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-
-                    <div className="wiserr-nav wiserr-nav-right">
-                        <ul className="nav-list" >
-
-                            <li className="display-from-md">
-                                <NavLink to="/business" className="business-nav-link nav-link">Wiserr Business</NavLink>
-                            </li>
-
-                            <li className="display-from-md">
-                                <div className="resources-tab-package">
-                                    <NavLink to="/categories" className="explore-nav-link nav-link">Explore</NavLink>
-                                </div>
-                            </li>
-
-                            <li className="display-from-sm">
-                                <NavLink to="/login" rel="nofollow" className="js-open-popup-login nav-link">Login/Join</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
                 </div>
             </header >
         </div>
--- src/pages/gig-details.jsx
+++ src/pages/gig-details.jsx
@@ -7,8 +7,8 @@ import { connect, useSelector, useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { getById } from '../store/actions/gigs.actions.js';
 import { GigReview } from '../cmps/gig-review.jsx';
-import { AppHeader} from '../cmps/headers/app-header.jsx'
-import { CategoriesNavHeader} from '../cmps/headers/categories-nav-header.jsx'
+import { AppHeader } from '../cmps/headers/app-header.jsx'
+import { CategoriesNavHeader } from '../cmps/headers/categories-nav-header.jsx'
 import { GreenVMark } from '../services/svg.service.js';
 
 
@@ -22,7 +22,7 @@ export const GigDetails = (props) => {
     const params = useParams()
 
     useEffect(() => {
-        console.log('params.gigId',params.gigId )
+        console.log('params.gigId', params.gigId)
         dispatch(getById(params.gigId))
     }, [params])
 
@@ -49,95 +49,116 @@ export const GigDetails = (props) => {
                         : <CategoriesNavHeader style={{ visibility: 'hidden' }}
                             onChangeCategory={onChangeCategory} />} */}
                     <AppHeader />
-                    <span className="line-sep"></span>
                     <CategoriesNavHeader />
-                    <span className="line-sep"></span>
                 </div>
             </div>
-            <section className="gig-details flex"> 
-                <div className="left-container">
-                    <section className="gig-info">
-                        <h3 className="text-display">{gig.title}</h3>
-                        <div className="owner-details flex">
-                            <img className="sml-round-img" src={`${gig.owner.imgUrl}`} alt="" /> &nbsp;
-                            <p className="owner-name">&nbsp;{gig.owner.fullName} &nbsp;</p>
-                        </div>
-                        <div className="img-container">
-                            <img className="gig-details-img img" src={`${gig.imgUrl[0]}`} alt="" />
-                        </div>
-                    </section>
-                    <section className="about-details">
-                        <h2 className="about-title">About this Gig</h2>
-                        <p className="about-this-gig">{gig.description.aboutThisGig}</p>
-                        <br />
-                        {whatYouGet &&<h3 className='about-get'>What Do You Get</h3>}
-                        {/* {gig.description.whatDoYouGet} && <p className="what-do-you-get"> */}
+            <section className="gig-details-container">
+                <div className="gig-details">
+                    <div className="left-container">
+                        <section className="gig-info">
+                            <h3 className="text-display">{gig.title}</h3>
+                            <div className="owner-details">
+                                <img className="sml-round-img" src={`${gig.owner.imgUrl}`} alt="" /> &nbsp;
+                                <p className="owner-name">&nbsp;{gig.owner.fullName} &nbsp;</p>
+                            </div>
+                            <div className="img-container">
+                                <img className="gig-details-img img" src={`${gig.imgUrl[0]}`} alt="" />
+                            </div>
+                        </section>
+                        <section className="about-details">
+                            <h2 className="about-title">About this Gig</h2>
+                            <p className="about-this-gig">{gig.description.aboutThisGig}</p>
+                            <br />
+                            {whatYouGet && <h3 className='about-get'>What Do You Get</h3>}
+                            {/* {gig.description.whatDoYouGet} && <p className="what-do-you-get"> */}
                             <dl className="what-do-you-get">
 
                                 {whatYouGet && whatYouGet.map(line => <dd>{line}</dd>)}
                             </dl>
-                        {/* </p> */}
-                        <article className="about-seller">
-                            <p className="about-title">About The Seller</p>
-                            <div className="owner-card flex">
-                                <img className="md-round-img" src={`${gig.owner.imgUrl}`} alt="" />
-                                <div className="owner-card-right flex">
-                                    <p>{gig.owner.fullName}</p>
-                                    <p><i className="fa fa-star filled">
-                                    <i className="fa fa-star filled">
-                                        <i className="fa fa-star filled">
+                            {/* </p> */}
+                            <article className="about-seller">
+                                <p className="about-title">About The Seller</p>
+                                <div className="owner-card">
+                                    <img className="md-round-img" src={`${gig.owner.imgUrl}`} alt="" />
+                                    <div className="owner-card-right">
+                                        <p>{gig.owner.fullName}</p>
+                                        <p><i className="fa fa-star filled">
                                             <i className="fa fa-star filled">
                                                 <i className="fa fa-star filled">
-                                                    5</i></i></i></i></i>
+                                                    <i className="fa fa-star filled">
+                                                        <i className="fa fa-star filled">
+                                                            5</i></i></i></i></i>
 
-                                </p>
-                            </div>
-                        </div>
-                        <div className="owner-info">
-                            <ul className="info-left flax">
-                                <li>
-                                    From
-                                    <strong>Israel</strong>
-                                </li>
-                                <li>
-                                    Member Since
-                                    <strong>2 month ago</strong>
-                                </li>
-                                <li>
-                                    Avg response time
-                                    <strong>3 hours</strong>
-                                </li>
-                                <li>
-                                    Last Delivery
-                                    <strong>About 2 Hours ago</strong>
-                                </li>
-                            </ul>
-                            <article className="seller-description">
-                                <div className="owner-summary">Hello I am Freelance Graphic Design and Illustrator based in Israel, I have working in graphic design industry for almost 6 years.</div>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="owner-info">
+                                    <ul className="info-left flax">
+                                        <li>
+                                            From
+                                            <strong>Israel</strong>
+                                        </li>
+                                        <li>
+                                            Member Since
+                                            <strong>2 month ago</strong>
+                                        </li>
+                                        <li>
+                                            Avg response time
+                                            <strong>3 hours</strong>
+                                        </li>
+                                        <li>
+                                            Last Delivery
+                                            <strong>About 2 Hours ago</strong>
+                                        </li>
+                                    </ul>
+                                    <article className="seller-description">
+                                        <div className="owner-summary">Hello I am Freelance Graphic Design and Illustrator based in Israel, I have working in graphic design industry for almost 6 years.</div>
+                                    </article>
+                                </div>
                             </article>
-                        </div>
-                    </article>
-                </section>
-                <section className="reviews">
+                        </section>
+                        <section className="reviews">
 
-                    {gig.reviews.map(review => {
-                        return <article key={review.id}>
-                            <GigReview review={review} />
-                        </article>
-                    })}
-                </section>
-            </div>
-            <div className="call-to-action flex">
-                <div className="price-package">Special Offer</div>
-                <div className="order-title-wrapper flex">
-                    <span className="order-price">${gig.price}</span>
-                    <p className="order-subtitle">{gig.title}</p>
-                    {gig.description.littleDetails && <dl> {gig.description.littleDetails.map(detail => <dt className='littleDetails'><GreenVMark/>{detail}</dt>)}</dl>}
-                    <button className="buy-btn">Continue</button>
+                            {gig.reviews.map(review => {
+                                return <article key={review.id}>
+                                    <GigReview review={review} />
+                                </article>
+                            })}
+                        </section>
+                    </div>
+
+                    <div className="sticky-outer-wrapper-gig-buy">
+                        <div className="sticky-inner-wrapper-gig-buy">
+                            <aside class="sidebar-content">
+                                {/* <div class="packages-tabs triple"> */}
+                                {/* <input id="package-tab-1" name="package-tab-group" type="radio" checked=""> */}
+                                {/* <input id="package-tab-2" name="package-tab-group" type="radio"> */}
+                                {/* <input id="package-tab-3" name="package-tab-group" type="radio"> */}
+                                {/* <div class="nav-container">
+            <label for="package-tab-1">Basic</label>
+        <label for="package-tab-2" class="">Standard</label>
+        <label for="package-tab-3" class="">Premium</label> */}
+                                {/* </div> */}
+                                <div className="call-to-action">
+                                    <div className="price-package">Special Offer</div>
+                                    <div className="order-title-wrapper">
+                                        <span className="order-price">${gig.price}</span>
+                                        <p className="order-subtitle">{gig.title}</p>
+                                        {gig.description.littleDetails && <dl> {gig.description.littleDetails.map(detail => <dt className='littleDetails'><GreenVMark />{detail}</dt>)}</dl>}
+
+                                        <footer>
+                                            <button className="buy-btn">Continue ( {gig.price} )</button>
+                                        </footer>
+                                    </div>
+
+                                    <button className="contact-seller">Contact Seller</button>
+                                </div>
+                            </aside>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </section>
-        </React.Fragment>
+            </section >
+        </React.Fragment >
     )
 
 }
