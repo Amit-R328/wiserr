c16c4cb2666a641cd9aaab817f6ae7cdc4719143
Sharon Bello <sharonbellotech@gmail.com>
2022-05-31 12:57:58 +0300

    test4

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,7 +3,7 @@ import { CategoriesNavHeader } from '../cmps/headers/categories-nav-header.jsx'
 import { HeroHeaderHomePage } from '../cmps/headers/hero-header-homepage.jsx'
 import { AppHeaderHomePage } from '../cmps/headers/app-header-homepage.jsx'
 import { HomepageCategoriesCarouselTest } from '../cmps/homepage-sections/homepage-categories-carousel-copy.jsx'
-import { HomepageCategoriesCarousel } from '../cmps/homepage-sections/homepage-categories-carousel.jsx'
+// import { HomepageCategoriesCarousel } from '../cmps/homepage-sections/homepage-categories-carousel.jsx'
 import { SellingPropHomepage } from '../cmps/homepage-sections/selling-prop-homepage.jsx'
 import { TestimonialsHomepage } from '../cmps/homepage-sections/testimonials-homepage.jsx'
 
