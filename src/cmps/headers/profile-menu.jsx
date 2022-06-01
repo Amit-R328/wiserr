6a1f8e39ea41ade266b51b032ce032ccd066ee0a
rinat <rinat.brandes@gmail.com>
2022-06-01 18:16:30 +0300

    seller route

--- src/cmps/headers/profile-menu.jsx
+++ src/cmps/headers/profile-menu.jsx
@@ -5,10 +5,10 @@ export const ProfileMenu = ({onLogout, user, closeMenu}) => {
         <div className="profile-menu">
             <div className="Menu-subcategory"></div>
             <ul className="sub-category clean-list">
-            <li className="menu-item" onClick={() => closeMenu()}><NavLink  to={`/business/add-gig`}>Add gig</NavLink></li>
+            <li className="menu-item" onClick={() => closeMenu()}><NavLink  to={`/seller/add-gig`}>Add gig</NavLink></li>
             <li className="menu-item" onClick={() => closeMenu()}><NavLink to={`/profile/${user._id}`}>User Profile</NavLink></li>
             {/* <li className="menu-item" onClick={() => closeMenu()}><NavLink to={`/business/dashboard/${user._id}`}>Dashboard</NavLink></li> */}
-            <li className="menu-item logout" onClick={() => onLogout()}><NavLink to={`/`}>Puki</NavLink></li>
+            <li className="menu-item logout" onClick={() => onLogout()}><NavLink to={`/`}>Logout</NavLink></li>
             </ul>
         </div>
     )
