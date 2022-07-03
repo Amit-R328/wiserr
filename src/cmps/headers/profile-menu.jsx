7a77a0035bffd506e65f5e0b40a56eda12344495
sharon bello <sharonbellotech@gmail.com>
2022-07-03 19:29:23 +0300

    profile-menu cleanup

--- src/cmps/headers/profile-menu.jsx
+++ src/cmps/headers/profile-menu.jsx
@@ -5,11 +5,9 @@ export const ProfileMenu = ({ onLogout, user, closeMenu }) => {
     return (
         <section className="profile-menu-wrapper">
             <div className="profile-menu">
-                {/* <div className="Menu-subcategory"></div> */}
                 <ul className="sub-category clean-list">
                     <li className="menu-item" onClick={() => closeMenu()}><NavLink to={`/seller/add-gig`}>Add gig</NavLink></li>
                     {user && <li className="menu-item" onClick={() => closeMenu()}><NavLink to={`/profile/${user._id}`}>User Profile</NavLink></li>}
-                    {/* <li className="menu-item" onClick={() => closeMenu()}><NavLink to={`/seller/dashboard/${user._id}`}>Dashboard</NavLink></li> */}
                     <li className="menu-item logout" onClick={() => onLogout()}><NavLink to={`/`}>Logout</NavLink></li>
                 </ul>
             </div>
