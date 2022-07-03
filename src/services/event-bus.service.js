86d98726749cfb36a9c5ba8537b7515f421c69bf
sharon bello <sharonbellotech@gmail.com>
2022-07-03 19:46:49 +0300

    event-bus.service cleanup

--- src/services/event-bus.service.js
+++ src/services/event-bus.service.js
@@ -1,11 +1,8 @@
 function on(eventName, listener) {
-
     const callListener = ({ detail }) => {
         listener(detail)
     }
-
     window.addEventListener(eventName, callListener)
-
     return () => {
         window.removeEventListener(eventName, callListener)
     }
@@ -16,14 +13,14 @@ function emit(eventName, data) {
 }
 
 export const eventBusService = { on, emit }
-
 export function showUserMsg(txt, type = '') {
     eventBusService.emit('show-user-msg', { txt, type })
 }
+
 export function showSuccessMsg(txt) {
-    
     showUserMsg(txt, 'success')
 }
+
 export function showErrorMsg(txt) {
     showUserMsg(txt, 'danger')
 }
