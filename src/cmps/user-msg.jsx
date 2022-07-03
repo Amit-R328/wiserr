82de6f6221b07a4ce70a357993ca430b0a6bf845
sharon bello <sharonbellotech@gmail.com>
2022-07-03 16:37:00 +0300

    user-msg clean

--- src/cmps/user-msg.jsx
+++ src/cmps/user-msg.jsx
@@ -8,7 +8,7 @@ export class UserMsg extends React.Component {
   }
 
   componentDidMount() {
-    // Here we listen to the event that we emited, its important to remove the listener 
+    // Here we listen to the event that we emitted, it's important to remove the listener 
     this.removeEvent = eventBusService.on('show-user-msg', (msg) => {
       this.setState({ msg })
       setTimeout(() => {
