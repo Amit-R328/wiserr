040e2a232d711566f2dd23ff66039ff96315e0f7
Amit <amitrozen2015@gmail.com>
2022-07-03 11:25:51 +0300

    .

--- src/pages/user-profile.jsx
+++ src/pages/user-profile.jsx
@@ -26,7 +26,7 @@ export const UserProfile = () => {
         dispatch(loadOrders(loggedInUser, 'getBuys'))
         setTimeout(() =>{
             setLoader(false)
-        }, 3000)
+        }, 3700)
         // console.log(orders)
         orders = orders.filter(order => order.seller !== loggedInUser.userName)
     }, [])
