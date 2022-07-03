44d628cef33ea596c0bb763b6459fd8eaee46275
sharon bello <sharonbellotech@gmail.com>
2022-07-03 19:13:43 +0300

    categories-carousel refactor

--- src/cmps/homepage-sections/categories-carousel.jsx
+++ src/cmps/homepage-sections/categories-carousel.jsx
@@ -1,14 +1,45 @@
 import React from "react"
 import { loadGigs, setFilter } from '../../store/actions/gig.actions.js'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 
 export const CategoriesCarousel = () => {
-
     let { filterBy } = useSelector((storeState) => storeState.gigModule)
-    const { gigs } = useSelector((storeState) => storeState.gigModule)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
+    let slideCategories = [
+        {
+            elementName: 'Graphics & Design',
+            legends: 'Build your brand',
+            title: 'Logo Design',
+            img: 'https://res.cloudinary.com/dcbbqlssh/image/upload/v1656862448/logo-design-2x_zmqxkh.webp'
+        },
+        {
+            elementName: 'Digital Marketing',
+            legends: 'Customize your site',
+            title: 'WordPress',
+            img: 'https://res.cloudinary.com/dcbbqlssh/image/upload/v1656862504/wordpress-2x_ykq9xd.webp'
+        },
+        {
+            elementName: 'Business',
+            legends: 'Learn your business',
+            title: 'Data Entry',
+            img: 'https://res.cloudinary.com/dcbbqlssh/image/upload/v1656862554/data-entry-2x_fv7elm.webp'
+        },
+        {
+            elementName: 'Video & Animation',
+            legends: 'Engage your audience',
+            title: 'Video Explainer',
+            img: 'https://res.cloudinary.com/dcbbqlssh/image/upload/v1656862592/animated-explainer-2x_fm4q6h.jpg'
+        },
+        {
+            elementName: 'Digital Marketing',
+            legends: 'Reach more customers',
+            title: 'Social Media',
+            img: 'https://res.cloudinary.com/dcbbqlssh/image/upload/v1656862621/social-2x_crn0um.webp'
+        }
+    ]
 
     const onChangeCategory = (category) => {
         filterBy = { txt: '', priceMin: 0, priceMax: Infinity, deliveryDate: 0, category: category }
@@ -19,45 +50,19 @@ export const CategoriesCarousel = () => {
 
     return (
         <div className="slider-container">
-            <div className="slide" onClick={() => onChangeCategory('Graphics & Design')}>
-                <h4 className="category-legends">
-                    <small>Build your brand</small><br></br>
-                    <span>Logo Design</span>
-                </h4>
-                <img alt="Logo Design" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png" srcSet="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png 2x" />
-            </div>
-
-            <div className="slide" onClick={() => onChangeCategory('Digital Marketing')}>
-                <h4 className="category-legends">
-                    <small>Customize your site</small><br></br><span>WordPress</span></h4>
-                <img alt="WordPress" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png" srcSet="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png 2x" />
-            </div>
-
-            <div className="slide" onClick={() => onChangeCategory('Business')}>
-                <h4 className="category-legends">
-                    <small>Learn your business</small><br></br><span>Data Entry</span></h4>
-                <img alt="Data Entry" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/data-entry-2x.png" />
-            </div>
-
-            {/* <div className="slide" onClick={() => onChangeCategory('Music & Audio')}>
-                <h4 className="category-legends">
-                    <small>Share your message</small><br></br><span>Voice Over</span></h4>
-                <img alt="Voice Over" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png" srcSet="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png 2x" />
-            </div> */}
-
-            <div className="slide" onClick={() => onChangeCategory('Video & Animation')}>
-                <h4 className="category-legends">
-                    <small>Engage your audience</small><br></br><span>Video Explainer</span></h4>
-                <img alt="Video Explainer" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png" data-srcset="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png 2x" className=" ls-is-cached lazyloaded" srcSet="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png 2x" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png" />
-            </div>
-
-            <div className="slide" onClick={() => onChangeCategory('Digital Marketing')}>
-                <h4 className="category-legends">
-                    <small>Reach more customers</small><br></br><span>Social Media</span></h4>
-                <img alt="Social Media" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png" data-srcset="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png 2x" className=" ls-is-cached lazyloaded" srcSet="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png 1x, https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_2.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png 2x" src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png" />
-            </div>
+            <ul className="slider-categories-list">
+                {slideCategories.map(category =>
+                    <li key={category}>
+                        <div className="slide" onClick={() => onChangeCategory(`${category.elementName}`)}>
+                            <h4 className="category-legends"><small>{category.legends}</small><br></br><span>{category.title}</span></h4>
+                            <img alt={category.title} src={category.img} />
+                        </div>
+                    </li>)}
+            </ul>
         </div>
     )
 }
+
+
 
 
