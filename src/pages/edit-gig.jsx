4be1157661f274601c86f0b13c2b1c0854dccbc7
Amit <amitrozen2015@gmail.com>
2022-07-09 12:45:09 +0300

    edit gig final fix

--- src/pages/edit-gig.jsx
+++ src/pages/edit-gig.jsx
@@ -3,20 +3,12 @@ import { useParams } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { getById, updateGig } from '../store/actions/gig.actions.js'
+import { updateGig } from '../store/actions/gig.actions.js'
 import { gigService } from '../services/gig.service'
-import { EditGigInput } from '../cmps/edit-input.jsx'
 
 export const EditGig = (props) => {
-    // const { gig } = useSelector((storeState) => storeState.gigModule)
-    // const { loggedInUser } = useSelector((storeState) => storeState.userModule)
-    // const { orders } = useSelector((storeState) => storeState.orderModule)
-    // const [user, setUser] = useState({})
-    const description = { aboutThisGig: '', whyUs: '', whatDoYouGet: '' }
-    const initialGig = {
-        title: '', imgUrl: '', description: description
-    }
-    const [currGig, setCurrGig] = useState(initialGig)
+    const { gig } = useSelector((storeState) => storeState.gigModule)
+    const [currGig, setCurrGig] = useState(gig)
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const params = useParams()
@@ -31,7 +23,6 @@ export const EditGig = (props) => {
         if (params.gigId) {
             fetchGig()
         }
-        // const gig = getById(params.gigId)
         window.scrollTo(0, 0)
     }, [params])
 
@@ -40,34 +31,16 @@ export const EditGig = (props) => {
     const handleChange = (ev) => {
         let value = ev.target.value
         const name = ev.target.name
-        // console.log('value', value)
-        // console.log('name', name)
         if (name === 'aboutThisGig' || name === 'whyUs' || name === 'whatDoYouGet') {
-            // console.log('in if')
-            setCurrGig({ ...currGig, description: { ...description, [name]: value } })
+            setCurrGig({ ...currGig, description: { ...currGig.description, [name]: value } })
         } else {
             setCurrGig({ ...currGig, [name]: value })
         }
-        // console.log('currGig', currGig)
     }
 
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        // const data = new FormData(event.currentTarget)
-
-        // const description = { 
-        //     aboutThisGig: data.get('aboutThisGig'),
-        //     whyUs: data.get('whyUs'),
-        //     whatDoYouGet: data.get('whatDoYouGet')
-        // }
-        // const currGig = {
-        //     _id: currGig._id,
-        //     title: data.get('title'),
-        //     imgUrl: data.get('imgUrl'),
-        //     description: description
-        // }
-
         console.log('currGig', currGig)
         dispatch(updateGig(currGig))
         navigate(-1)
@@ -78,7 +51,6 @@ export const EditGig = (props) => {
     if (currGig.description && currGig.description.whatDoYouGet) {
         whatYouGet = currGig.description.whatDoYouGet.split('\n')
     }
-    // console.log('currGig.description.aboutThisGig', currGig.description.aboutThisGig)
 
     return (
         <section className="edit-gig-details-container container">
@@ -96,15 +68,14 @@ export const EditGig = (props) => {
                     <label className="edit-gig-label">Gig Image</label>
                     <img className="edit-gig-img" src={currGig.imgUrl[0]} alt="Gig img"></img>
                 </div>
-                {console.log('currGig description', currGig.description)}
-                <EditGigInput inputRef="aboutThisGig" label="About This Gig" name="aboutThisGig" description={currGig.description.aboutThisGig} handleChange={handleChange} />
+                {/* <EditGigInput inputRef="aboutThisGig" label="About This Gig" name="aboutThisGig" description={currGig.description.aboutThisGig} handleChange={handleChange} />
                 <EditGigInput inputRef="whyUs" label="Why Us" name="whyUs" description={currGig.description.whyUs} handleChange={handleChange} />
-                <EditGigInput inputRef="whatYouGet" label="What Do You Get" name="whatYouGet" description={whatYouGet} handleChange={handleChange} /> 
-                {/* <div className="edit-gig-input-container">
+                <EditGigInput inputRef="whatYouGet" label="What Do You Get" name="whatYouGet" description={whatYouGet} handleChange={handleChange} />  */}
+                <div className="edit-gig-input-container">
                     <label className="edit-gig-label">About This Gig</label>
                     <input className="edit-gig-input" name="aboutThisGig" type="search" value={currGig.description.aboutThisGig} onChange={handleChange} />
-                </div> */}
-                {/* <div className="edit-gig-input-container">
+                </div> 
+                <div className="edit-gig-input-container">
                     <label className="edit-gig-label">Why Us</label>
                     <input className="edit-gig-input" name="whyUs" type="search" value={currGig.description.whyUs} onChange={handleChange} />
                 </div>
@@ -112,9 +83,9 @@ export const EditGig = (props) => {
                 <div className="edit-gig-input-container">
                     <label className="edit-gig-label">What Do You Get</label>
                     <input className="edit-gig-input" name="whatDoYouGet" type="search" value={whatYouGet} onChange={handleChange} />
-                </div> */}
+                </div>
 
-                {/* {currGig.description.littleDetails && <div className="edit-gig-littleDetails">
+                {/* {/* {currGig.description.littleDetails && <div className="edit-gig-littleDetails">
                         <label className="edit-gig-label">Little Details</label>  
                         <textarea className="edit-gig-textarea" rows={5} value={currGig.description.littleDetails.map((detail) => detail)}>
                             {<dl className="edit-gig-littleDetails"> {gig.description.littleDetails.map((detail, idx) => <dd className='littleDetails' key={idx}>{detail}</dd>)}</dl>}
