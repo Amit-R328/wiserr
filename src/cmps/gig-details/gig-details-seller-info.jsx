7f79ad6917a2d606e298e9442cc89ee18223c7c7
Amit <amitrozen2015@gmail.com>
2022-07-09 11:50:31 +0300

    small fixes gig-details

--- src/cmps/gig-details/gig-details-seller-info.jsx
+++ src/cmps/gig-details/gig-details-seller-info.jsx
@@ -1,32 +1,34 @@
 import ImageGallery from 'react-image-gallery'
 
-export const GigDetailsSellerInfo = ({gig, user, orders}) => {
-    
-   
+export const GigDetailsSellerInfo = ({ gig, user, orders }) => {
+
+
     let images
     if (gig && gig.imgUrl) {
         images = gig.imgUrl.map((img) => {
             return { original: img, thumbnail: img }
         })
     }
-    
+
     return (
         <section className="gig-info">
-        <h1 className="text-display">{gig.title}</h1>
-        <div className="owner-details owner-container" >
-            <img className="sml-round-img" src={`${gig.owner.imgUrl}`} alt="" /> &nbsp;
-            <p className="owner-name">&nbsp;{gig.owner.fullName} &nbsp;</p>
-            <p className='owner-level'>{user.level ? user.level : 'Level 1 Seller '}&nbsp;| </p>
-            <p className='owner-order-qty'> {orders.length ? `   ${orders.length} Order in Queue |` : ''}</p>
-            <div className="gig-rate">
-                {gig.reviews.length ? <div className="avg-rate">{((gig.reviews.reduce((acc, review) => acc + (review.stars), 0)) / gig.reviews.length).toFixed(1)}<p className="rate-reviews-qty">({gig.reviewsQty})</p></div> :
-                    <div className="avg-rate">4.9</div>}
-            </div>
+            <h1 className="text-display">{gig.title}</h1>
+            <div className="owner-details owner-container" >
+                <div className="owner-name-and-img">
+                    <img className="sml-round-img" src={`${gig.owner.imgUrl}`} alt="" /> &nbsp;
+                    <p className="owner-name">{gig.owner.fullName}</p>
+                </div>
+                <p className='owner-level'>{user.level ? user.level : 'Level 1 Seller '} </p>
+                <p className='owner-order-qty'> {orders.length ? `   ${orders.length} Order in Queue ` : ''}</p>
+                <div className="gig-rate">
+                    {gig.reviews.length ? <div className="avg-rate">{((gig.reviews.reduce((acc, review) => acc + (review.stars), 0)) / gig.reviews.length).toFixed(1)}<p className="rate-reviews-qty">({gig.reviewsQty})</p></div> :
+                        <div className="avg-rate">4.9</div>}
+                </div>
 
-        </div>
-        <div className="gig-details-img-container">
-            <ImageGallery showThumbnails={false} showPlayButton={false} items={images} />
-        </div>
-    </section>
+            </div>
+            <div className="gig-details-img-container">
+                <ImageGallery showThumbnails={false} showPlayButton={false} items={images} />
+            </div>
+        </section>
     )
 }
